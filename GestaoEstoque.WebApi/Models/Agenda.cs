67d79e7b03d4d8a0376e2101e790ed1a0f886664
ThiagoTSilva <anthiagotavares@gmail.com>
2020-11-21 22:48:19 -0300

    Criando api de gestão de controle.

---
+++ GestaoEstoque.WebApi/Models/Agenda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoEstoque.WebApi.Models
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public DateTime DhInicio { get; set; }
+        public DateTime DhFinal { get; set; }
+    }
+}
---
+++ GestaoEstoque.WebApi/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoEstoque.WebApi.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Senha { get; set; }
+        public DateTime DhCadastro { get; set; } 
+        public DateTime DhAtualizacao { get; set; }
+        public bool Status { get; set; }
+    }
+}
