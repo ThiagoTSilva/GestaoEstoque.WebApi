b2c43a852916be0a252bffff90c66df5bf2ad0f9
ThiagoTSilva <anthiagotavares@gmail.com>
2020-11-23 17:54:21 -0300

    Incluindo filtro na consulta de usuario

--- GestaoEstoque.WebApi/Repositories/UsuarioRepository.cs
+++ GestaoEstoque.WebApi/Repositories/UsuarioRepository.cs
@@ -2,15 +2,26 @@
 using GestaoEstoque.WebApi.Models;
 using GestaoEstoque.WebApi.Repositories.Interface;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GestaoEstoque.WebApi.Repositories
 {
     public class UsuarioRepository : BaseRepository<Usuario>, IUsuarioRepository
     {
+        private Context _context;
         public UsuarioRepository(Context context)
          : base(context)
         {
+            _context = context;
+        }
+
+        public Usuario UsuarioValido(int id, string senha)
+        {
+            IQueryable<Usuario> query = _context.Usuarios;
+            query = query.Where(u => u.Senha == senha && u.Id == id);
+
+            return (Usuario)query;
         }
     }
 }
